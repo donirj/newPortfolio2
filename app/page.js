18c3916399e1c44dfccf3dd2ce3bbd8be475a3a3
Andoni <andoniresendiz@gmail.com>
2023-12-19 00:23:02 -0600

    first commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import HeroSection from './../HeroSection'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
+      <HeroSection />
+      
+      
+    </main>
+  )
+}
